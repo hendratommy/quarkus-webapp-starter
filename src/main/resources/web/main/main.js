2786efbfe4af427baf927ad0c17a19052aea2744
Hendra Tommy Wijaya <hendra.tommy.w@gmail.com>
2024-12-27 11:29:03 +0700

    add fouce

--- src/main/resources/web/main/main.js
+++ src/main/resources/web/main/main.js
@@ -1,7 +1,7 @@
-import { setBasePath } from '@shoelace-style/shoelace';
+import { setBasePath } from "@shoelace-style/shoelace";
 
 setBasePath("/static/shoelace");
 
-import '@shoelace-style/shoelace/dist/components/input/input.js';
-import '@shoelace-style/shoelace/dist/components/button/button.js';
-import '@shoelace-style/shoelace/dist/components/drawer/drawer.js';
+import "@shoelace-style/shoelace/dist/components/input/input.js";
+import "@shoelace-style/shoelace/dist/components/button/button.js";
+import "@shoelace-style/shoelace/dist/components/drawer/drawer.js";
